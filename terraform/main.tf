e6115fa1fb39913caa47e622df584b002b944907
S Bhavya <b@gmail.com>
2026-02-02 03:08:46 +0530

    Initial commit: Terraform S3 + CloudFront static website

---
+++ terraform/main.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+  region = var.region
+}
+
+# -------------------------
+# S3 Bucket (PRIVATE)
+# -------------------------
+resource "aws_s3_bucket" "website" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# -------------------------
+# Upload index.html
+# -------------------------
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "../website/index.html"
+  content_type = "text/html"
+}
+
+# -------------------------
+# CloudFront Origin Access Control
+# -------------------------
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "interiors-oac"
+  description                       = "OAC for S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# -------------------------
+# CloudFront Distribution
+# -------------------------
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+# -------------------------
+# Allow CloudFront to read S3
+# -------------------------
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
+}
